40a87d534e5c60f95da338d1e0443cf3ec827760
zaramagasoft <65206037+zaramagasoft@users.noreply.github.com>
2020-05-12 17:41:05 +0200

    SPLASH & edittextComprobar 3

---
+++ app/src/main/java/com/alb/fibererte/ui/main/LogicaNormal.java
@@ -0,0 +1,87 @@
+package com.alb.fibererte.ui.main;
+
+public class LogicaNormal {
+
+
+    //turnos
+    public double turno1=0;
+    public double turno2=45.52;
+    public double turno3=135.42;
+    //Salarios2020
+    public double grupo1b=19064.71;
+    public double grupo2=22271.89;
+    public double grupo3=24250.1;
+    public double grupo4=28019.75;
+    public double grupo5=31881.74;
+    //tablasSepe2020
+    public double sepehijos0=1098.09;
+    public double sepehijos1=1254.96;
+    public double sepehijos2=1411.83;
+    //totales
+    public double totalSepe=0;
+    public double totalFiber=0;
+
+    public LogicaNormal() {
+
+    }
+
+    public double getTurno1() {
+        return turno1;
+    }
+
+    public double getTurno2() {
+        return turno2;
+    }
+
+    public double getTurno3() {
+        return turno3;
+    }
+
+    public double getGrupo1b() {
+        return grupo1b;
+    }
+
+    public double getGrupo2() {
+        return grupo2;
+    }
+
+    public double getGrupo3() {
+        return grupo3;
+    }
+
+    public double getGrupo4() {
+        return grupo4;
+    }
+
+    public double getGrupo5() {
+        return grupo5;
+    }
+
+    public double getSepehijos0() {
+        return sepehijos0;
+    }
+
+    public double getSepehijos1() {
+        return sepehijos1;
+    }
+
+    public double getSepehijos2() {
+        return sepehijos2;
+    }
+
+    public double getTotalSepe() {
+        return totalSepe;
+    }
+
+    public void setTotalSepe(double totalSepe) {
+        this.totalSepe = totalSepe;
+    }
+
+    public double getTotalFiber() {
+        return totalFiber;
+    }
+
+    public void setTotalFiber(double totalFiber) {
+        this.totalFiber = totalFiber;
+    }
+}
